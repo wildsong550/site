0e9416038b702bccd9135fddee0916a72b57cc5d
wildsong550 <wildsong550@gmail.com>
2024-06-11 21:32:52 -0400

    Add files via upload
    criei primeira pagina html, escrevi java script, e css

---
+++ site/recursos/js/app.js
@@ -0,0 +1,28 @@
+Function forgotPasswordModa(){
+	let fade = document.querySelector('#fade')
+	let modal_card = document.querySelector('#modal_card')
+	
+	fade.classList.toggle('hide')
+	modal_card = document.querySelector('#modal_card')
+}
+
+Function createElementsinId(ElementHTML,ElementId,message){
+	let spanWrongPassword = document.createElement(ElementHTML)
+	
+	
+	
+	
+let cont = 0
+
+function registerlogin(){
+	
+	let login = localStorage.getItem("login"
+	if(login === null){
+		localStorage.setItem("login","icet2024")
+		localStorage.setItem("Password","icet2024")
+	}	
+}	
+
+function userValidation(){
+	
+	let loginStorage =  localStorage.getItem('login')
