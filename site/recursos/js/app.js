2afb8275bd9f248b626207da5fe35a701f7505d6
wildsong550 <wildsong550@gmail.com>
2024-06-12 21:17:11 -0400

    atualizacao do html

--- site/recursos/js/app.js
+++ site/recursos/js/app.js
@@ -1,28 +1,17 @@
-Function forgotPasswordModa(){
+function forgotPasswordModal(){
 	let fade = document.querySelector('#fade')
 	let modal_card = document.querySelector('#modal_card')
 	
 	fade.classList.toggle('hide')
-	modal_card = document.querySelector('#modal_card')
+	modal_card.classList.toggle('hide')
 }
 
-Function createElementsinId(ElementHTML,ElementId,message){
-	let spanWrongPassword = document.createElement(ElementHTML)
-	
-	
-	
-	
-let cont = 0
 
 function registerlogin(){
 	
-	let login = localStorage.getItem("login"
+	let login = localStorage.getItem("login")
 	if(login === null){
 		localStorage.setItem("login","icet2024")
 		localStorage.setItem("Password","icet2024")
 	}	
 }	
-
-function userValidation(){
-	
-	let loginStorage =  localStorage.getItem('login')
